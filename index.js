a554297bb787907efa769df6f9495e725d9b0a08
nerdydawin <nerdydawin@gmail.com>
2022-09-06 20:36:18 +0700

    dfdflll

--- index.js
+++ index.js
@@ -26,14 +26,12 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-if (process.env.NODE_ENC === 'production') {
-  const path = require('path')
-  // app.use(express.static('./client/build'))
-  app.use(express.static(path.join(__dirname, 'client/build/index.html')));
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
   
 
