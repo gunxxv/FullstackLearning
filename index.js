ce41207883cd12377eedc7fe2d2bc99b9b26b418
NerdyDawin <nerdy@email.com>
2022-09-06 17:49:25 +0700

    new deploy4

--- index.js
+++ index.js
@@ -27,21 +27,12 @@ require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
 if (process.env.NODE_ENC === 'production') {
-  // app.use(express.static('client/build'))
+  app.use(express.static('client/build'))
 
-  // const path = require('path')
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  // })
-  const path = require("path");
- 
-  // serve production assets e.g. main.js if route exists
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
- 
-  // serve index.html if route is not recognized
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
   
 
